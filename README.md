0b7b91e167ae8f336675720d0ab331456882860c
a0k00xa <ambreen.khan@walmart.com>
2021-01-31 16:58:29 -0500

    kids movie site

---
+++ README.md
@@ -0,0 +1,3 @@
+## Create a film database to showcase your favorite films.
+Create a Dashboard for all your favourite films. Each film should be displayed in in a box of its own with an image of the film of your choice, similar to how it would look on Netflix.
+
---
+++ index.js
@@ -0,0 +1,49 @@
+let menuLinks = document.getElementsByClassName("menu-link");
+let imageBanner = document.getElementById("img-banner-container");
+let movieContainer = document.getElementById("movie-container");
+
+for (let i=0; i<menuLinks.length; i++){
+    
+    menuLinks[i].addEventListener("click", function() {
+        let menu = menuLinks[i].textContent;
+        if(menu !== 'Home'){
+            imageBanner.style.display = "none";
+            movieContainer.style.display = "block"
+            getMovies(menu);
+        }
+        else {
+            imageBanner.style.display = "block";
+            movieContainer.style.display = "none"
+        }
+    })
+}
+
+
+function displayMovies(results) {
+
+    let movieHTML = results.map(result => {
+       if(result.poster_path){
+            let posterPath = `https://image.tmdb.org/t/p/w185_and_h278_bestv2/${result.poster_path}`
+            return (`
+                <div class="movie-card">
+                    <img class="movie-img" src="${posterPath}" />
+                </div>
+                `)
+       }
+    } ).join('')
+
+    movieContainer.innerHTML = `<div class="movies">${movieHTML}</div>`
+}
+
+
+async function getMovies(menu) {
+    let query = menu;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=e3bf556e2609779d62b837d5824b16ab&language=en-US&query=${query}&page=1&include_adult=false`;
+    let response = await fetch(url)
+    let data = await response.json()
+    displayMovies(data.results);
+}
+
+
+
+
